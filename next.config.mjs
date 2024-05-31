40ba19ae5302e1d26b1626f54535135a2e575d08
reza <reza1997karbakhsh@gmail.com>
2024-05-31 15:33:57 +0330

    env file

--- next.config.mjs
+++ next.config.mjs
@@ -17,6 +17,14 @@ const nextConfig = {
 
     env: {
       BASE_URL_API: 'http://localhost:8080',
+
+      NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:"pk_test_d2lzZS1jaGlja2VuLTAuY2xlcmsuYWNjb3VudHMuZGV2JA",
+      CLERK_SECRET_KEY:"sk_test_4ean5LsiDYLn3BuIreOKgqGGEiFTfkAS0DcVCrlDaS",
+      POSTGRES_URL:"postgres://default:idhEYcq9A3Os@ep-frosty-wood-a2pq1n54-pooler.eu-central-1.aws.neon.tech:5432/verceldb?sslmode=require",
+      POSTGRES_PRISMA_URL:"postgres://default:idhEYcq9A3Os@ep-frosty-wood-a2pq1n54-pooler.eu-central-1.aws.neon.tech:5432/verceldb?sslmode=require&pgbouncer=true&connect_timeout=15",
+      POSTGRES_URL_NO_SSL:"postgres://default:idhEYcq9A3Os@ep-frosty-wood-a2pq1n54-pooler.eu-central-1.aws.neon.tech:5432/verceldb",
+      POSTGRES_URL_NON_POOLING:"postgres://default:idhEYcq9A3Os@ep-frosty-wood-a2pq1n54.eu-central-1.aws.neon.tech:5432/verceldb?sslmode=require",
+
       },
 };
 
