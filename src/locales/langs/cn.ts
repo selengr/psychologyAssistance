b6a0f439344362e7d69f95d5e6e00f23148199e3
reza <reza1997karbakhsh@gmail.com>
2024-06-01 16:01:33 +0330

    up-to-date

--- src/locales/langs/cn.ts
+++ src/locales/langs/cn.ts
@@ -1,7 +1,6 @@
 // ----------------------------------------------------------------------
 
-// IF THIS TRANSLATION IS INCORRECT PLEASE IGNORE THIS AS THIS TRANSLATION IS FOR DEMO PURPOSES ONLY
-// We are happy if you can help improve the translation by sending an email to support@minimals.cc.
+// THIS TRANSLATION IS DEMO PURPOSES ONLY
 
 // ----------------------------------------------------------------------
 
@@ -9,11 +8,6 @@ const cn = {
   demo: {
     title: `Chinese`,
     introduction: `Lorem Ipsum 只是印刷和排版行业的虚拟文本。 自 1500 年代以来，Lorem Ipsum 一直是行业的标准虚拟文本，当时一位不知名的印刷商采用了一种类型的厨房并将其加扰以制作类型样本书。 它不仅经历了五个世纪，而且经历了电子排版的飞跃，基本保持不变。 它在 1960 年代随着包含 Lorem Ipsum 段落的 Letraset 表的发布而流行起来，最近还随着 Aldus PageMaker 等桌面出版软件（包括 Lorem Ipsum 的版本）而普及.`,
-  },
-  docs: {
-    hi: `你好`,
-    description: `需要帮忙？\n 请查看我们的文档.`,
-    documentation: `文件`,
   },
   app: `应用程序`,
   user: `用户`,
@@ -41,18 +35,6 @@ const cn = {
   analytics: `分析`,
   ecommerce: `电子商务`,
   management: `管理`,
-  menu_level: `菜单级别`,
-  menu_level_2a: `菜单级别 2a`,
-  menu_level_2b: `菜单级别 2b`,
-  menu_level_3a: `菜单级别 3a`,
-  menu_level_3b: `菜单级别 3b`,
-  menu_level_4a: `菜单级别 4a`,
-  menu_level_4b: `菜单级别 4b`,
-  item_disabled: `项目已禁用`,
-  item_label: `项目标签`,
-  item_caption: `项目标题`,
-  item_external_link: `项目外部链接`,
-  description: `描述`,
   other_cases: `其他情况`,
   item_by_roles: `按角色逐项`,
   only_admin_can_see_this_item: `只有管理员可以看到这个项目`,
--- src/locales/langs/fa.ts
+++ src/locales/langs/fa.ts
@@ -1,7 +1,6 @@
 // ----------------------------------------------------------------------
 
-// IF THIS TRANSLATION IS INCORRECT PLEASE IGNORE THIS AS THIS TRANSLATION IS FOR DEMO PURPOSES ONLY
-// We are happy if you can help improve the translation by sending an email to support@minimals.cc.
+// THIS TRANSLATION IS DEMO PURPOSES ONLY
 
 // ----------------------------------------------------------------------
 
@@ -41,18 +40,6 @@ const ir = {
     analytics: `التحليلات`,
     ecommerce: `کالا`,
     management: `مدیریت`,
-    menu_level: `مستوى القائمة `,
-    menu_level_2a: `مستوى القائمة 2 أ`,
-    menu_level_2b: `مستوى القائمة 2 ب`,
-    menu_level_3a: `مستوى القائمة 3 أ`,
-    menu_level_3b: `مستوى القائمة 3 ب`,
-    menu_level_4a: `مستوى القائمة 4 أ`,
-    menu_level_4b: `مستوى القائمة 4 ب`,
-    item_disabled: `العنصر معطل`,
-    item_label: `تسمية العنصر`,
-    item_caption: `عنوان العنصر`,
-    item_external_link: `رابط خارجي للمادة`,
-    description: `وصف`,
     other_cases: `حالات اخرى`,
     item_by_roles: `عنصر حسب الأدوار`,
     only_admin_can_see_this_item: `يمكن للمسؤول فقط رؤية هذا العنصر`,
