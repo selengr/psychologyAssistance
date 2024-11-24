baf75afe2c6414ae028d82a0e165b79d741aa15b
reza <reza1997karbakhsh@gmail.com>
2024-11-24 21:16:43 +0330

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,60 +1,71 @@
-'use client';
+// 'use client';
 
-import React, { useState } from 'react';
-import MenuPopover from '@/components/menu-popover/MenuPopover';
-import { MenuItem } from '@mui/material';
+// import React, { useState } from 'react';
+// import MenuPopover from '@/components/menu-popover/MenuPopover';
+// import { MenuItem } from '@mui/material';
 
-const Page = () => {
-  const [openPopover, setOpenPopover] = useState<HTMLElement | null>(null);
+// const Page = () => {
+//   const [openPopover, setOpenPopover] = useState<HTMLElement | null>(null);
 
-  const [openConfirm, setOpenConfirm] = useState(true);
+//   const [openConfirm, setOpenConfirm] = useState(true);
 
-  const handleOpenConfirm = () => {
-    setOpenConfirm(true);
-  };
+//   const handleOpenConfirm = () => {
+//     setOpenConfirm(true);
+//   };
 
-  const handleCloseConfirm = () => {
-    setOpenConfirm(false);
-  };
+//   const handleCloseConfirm = () => {
+//     setOpenConfirm(false);
+//   };
 
-  const handleOpenPopover = (event: React.MouseEvent<HTMLElement>) => {
-    setOpenPopover(event.currentTarget);
-  };
+//   const handleOpenPopover = (event: React.MouseEvent<HTMLElement>) => {
+//     setOpenPopover(event.currentTarget);
+//   };
 
-  const handleClosePopover = () => {
-    setOpenPopover(null);
-  };
+//   const handleClosePopover = () => {
+//     setOpenPopover(null);
+//   };
+//   return (
+//     <div>
+//       <MenuPopover
+//         open={openPopover}
+//         onClose={handleClosePopover}
+//         arrow="right-bottom"
+//         sx={{ width: 200, color: 'red !important' }}
+//       >
+//         <MenuItem
+//           //  onClick={() => {
+//           //       handleOpenConfirm();
+//           //       handleClosePopover();
+//           //  }}
+//           sx={{ color: 'error.main' }}
+//         >
+//           {/* <Iconify icon="eva:trash-2-outline" /> */}
+//           Delete
+//         </MenuItem>
+
+//         <MenuItem
+//         //  onClick={() => {
+//         //       // onEditRow();
+//         //       handleClosePopover();
+//         //  }}
+//         >
+//           {/* <Iconify icon="eva:edit-fill" /> */}
+//           Edit
+//         </MenuItem>
+//       </MenuPopover>
+//     </div>
+//   );
+// };
+
+// export default Page;
+
+
+const page = () => {
   return (
     <div>
-      <MenuPopover
-        open={openPopover}
-        onClose={handleClosePopover}
-        arrow="right-bottom"
-        sx={{ width: 200, color: 'red !important' }}
-      >
-        <MenuItem
-          //  onClick={() => {
-          //       handleOpenConfirm();
-          //       handleClosePopover();
-          //  }}
-          sx={{ color: 'error.main' }}
-        >
-          {/* <Iconify icon="eva:trash-2-outline" /> */}
-          Delete
-        </MenuItem>
-
-        <MenuItem
-        //  onClick={() => {
-        //       // onEditRow();
-        //       handleClosePopover();
-        //  }}
-        >
-          {/* <Iconify icon="eva:edit-fill" /> */}
-          Edit
-        </MenuItem>
-      </MenuPopover>
+      Enter
     </div>
   );
-};
+}
 
-export default Page;
+export default page;
